55baf1307b8aa1e0d337fb0682b3f0a11fd13df0
kazuki-kudo <kkudo@itgarage.jp>
2020-08-11 10:52:25 +0900

    add sleep.add dummy request.

--- reboot_rt.py
+++ reboot_rt.py
@@ -9,6 +9,7 @@
 # + RT_ADMIN_PASS : router admin password.  default 'password'
 
 import os,requests,sys,bs4
+import time
 
 import logging
 logging.basicConfig(level=logging.INFO, format=' %(asctime)s - %(levelname)s - %(message)s')
@@ -35,18 +36,24 @@ HOST_URL = 'http://' + rt_host + '/init.html'
 BASIC_AUTH = (rt_admin_user,rt_admin_pass)
 
 try:
+    # screen 0
+    # dummy request. possible 401 auth error.
+    res0 = requests.get(HOST_URL,auth=BASIC_AUTH)
+    time.sleep(2)
+
     # screen 1
     res1 = requests.get(HOST_URL,auth=BASIC_AUTH)
     res1.raise_for_status()
     
     html1 = bs4.BeautifulSoup(res1.text,'html.parser')
     ele = html1.select('input[name="nosave_session_num"]')[0]
-    
+    time.sleep(2)
+
     # screen 2
     nosave_session_num = ele.get('value')
     
     payload = {'nosave_reboot': '1', 'nosave_session_num': nosave_session_num}
-    res2 = requests.post(HOST_URL,data=payload,auth=BASIC_AUTH)
+    res2 = requests.post(HOST_URL + '?screen2',data=payload,auth=BASIC_AUTH)
     res2.raise_for_status()
 
     logging.info('compleate')
